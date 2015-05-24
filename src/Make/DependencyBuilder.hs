ea1921b9c9d01728cd84d0fde12ac9a77e4b80f5
Sahni <sahnib@amazon.com>
2015-05-24 11:00:34 +0530

    Dependency building integrated only upon timestamp changes

--- src/Make/DependencyBuilder.hs
+++ src/Make/DependencyBuilder.hs
@@ -1,3 +1,5 @@
+-- File: DependencyBuilder.hs
+-- Builds the dependencies for the target by looking at the timestamps.
 module Make.DependencyBuilder
 (
   ruleForTarget,
@@ -7,6 +9,7 @@ module Make.DependencyBuilder
 import Make.Primitives
 import System.Cmd
 import System.Exit
+import System.Directory
 
 ruleForTarget :: Target -> [Rule] -> Maybe Rule
 ruleForTarget t [] = Nothing
@@ -17,23 +20,43 @@ ruleForTarget t (x:xs) =
 
 runCommands :: [String] -> IO ExitCode
 runCommands [] = return ExitSuccess
-runCommands (x:xs) = do
-  exitCode <- system x
-  case exitCode of ExitSuccess -> runCommands xs
-                   ExitFailure a -> return $ ExitFailure a
+runCommands (x:xs) =
+  do
+    exitCode <- system x
+    case exitCode of ExitSuccess -> runCommands xs
+                     ExitFailure a -> return $ ExitFailure a
+
+-- Returns a true if target is older than the sources
+hasExpired :: Rule -> IO Bool
+hasExpired rule =
+  do
+    targetPresent <- doesFileExist $ target rule
+    sourcesPresentList <- sequence $ map doesFileExist (sources rule)
+    let allSourcesPresent = and sourcesPresentList
+    if (targetPresent && allSourcesPresent) then
+      return False
+    else
+      return True
 
 build :: Rule -> [Rule] -> IO ExitCode
-build rule ruleList = do
-  exitCode <-  executeAll ruleList (sources rule)
-  case exitCode of ExitSuccess -> runCommands (commands rule)
-                   ExitFailure a -> return $ ExitFailure a
+build rule ruleList =
+  do
+    exitCode <-  executeAll ruleList (sources rule)
+    rebuild <- hasExpired rule
+-- check here the timestamps of the sources as to if they are more recent than the target
+    if (rebuild) then
+      case exitCode of ExitSuccess -> runCommands (commands rule)
+                       ExitFailure a -> return $ ExitFailure a
+    else
+      return ExitSuccess
 
 executeAll :: [Rule] -> [Target] -> IO ExitCode
 executeAll _ [] = return ExitSuccess
-executeAll ruleList (target:otherTargets) = do
-  exitCode <- execute ruleList target
-  case exitCode of ExitSuccess -> executeAll ruleList otherTargets
-                   ExitFailure a -> exitWith $ ExitFailure a
+executeAll ruleList (target:otherTargets) =
+  do
+    exitCode <- execute ruleList target
+    case exitCode of ExitSuccess -> executeAll ruleList otherTargets
+                     ExitFailure a -> exitWith $ ExitFailure a
 
 execute :: [Rule] -> Target -> IO ExitCode
 execute ruleList target =
